31e525ebcf2b12e96db7ab2c7119ac686bfd5b58
Alexandre Garcia de Oliveira <romefeller@MacBook-Pro-de-Alexandre.local>
2019-12-08 15:32:01 -0200

    Day 8 complete

--- Day8.hs
+++ Day8.hs
@@ -11,7 +11,16 @@ imageLayers w t s = [map (\x -> take w (drop ((x-1)*w) s)) [1 .. t]]
 
 fewestZero :: [String] -> Int
 fewestZero ls = sum $ map (length . filter (=='0')) ls
-            
+        
+visible :: String -> Char
+visible = head . go . filter (/= '2') 
+    where 
+        go [] = ['2'] 
+        go xs = xs
+
+extract :: Int -> Int -> [[String]] -> Char
+extract i j ls = visible $ map (!! i) $ map (!! j) ls 
+
 main :: IO ()
 main = do 
     file <- readFile "d8"
@@ -20,3 +29,7 @@ main = do
     len <- return $ map fewestZero layers
     minLayer <- return . minimum $ len
     print $ layers !! ((\(Just x) -> x) $ findIndex (==minLayer) len)
+    -- pt2
+    vs <- return $ [extract i j layers | j <- [0 .. 5], i <- [0 .. 24]] 
+    print $ imageLayers 25 6 vs
+    
