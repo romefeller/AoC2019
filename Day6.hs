e949d5c1e3d6903bdec1d100d4d5ae0630f97752
Alexandre Garcia de Oliveira <romefeller@MacBook-Pro-de-Alexandre.local>
2019-12-09 00:12:43 -0200

    Day6 complete

--- Day6.hs
+++ Day6.hs
@@ -12,7 +12,7 @@ allPaths s paths = next
       next = case curr of
                   [] -> [[s]]
                   _ -> map (s:) $ concatMap (\x -> allPaths x paths) ns
-
+        
 main :: IO ()
 main = do 
     file <- readFile "d6"
@@ -21,3 +21,9 @@ main = do
     orbits <- return $ nub $ (map head p) ++ (map (!! 1) p)
     let numOrbits path x = length $ takeWhile (/= x) $ head $ filter (elem x) path
     print $ sum $ map (numOrbits (allPaths "COM" paths)) orbits
+    --pt 2
+    let you = (head $ filter (elem "YOU") (allPaths "COM" paths))
+        san = (head $ filter (elem "SAN") (allPaths "COM" paths))
+    inter <- return $ last $ intersect you san
+    print $ length (nub $ dropWhile (/= inter) you ++ dropWhile (/= inter) san) - 3
+    
