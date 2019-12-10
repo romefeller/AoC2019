95b45629ad63ece554456fed57f2825922cbd4d9
Alexandre Garcia de Oliveira <romefeller@MacBook-Pro-de-Alexandre.local>
2019-12-10 15:36:48 -0200

    Day10 pt1

---
+++ Day10.hs
@@ -0,0 +1,53 @@
+module Day10 where
+
+import Data.List
+
+testGrid :: [String]
+testGrid =     [".#..#"
+               ,"....."
+               ,"#####"
+               ,"....#"
+               ,"...##"
+               ]
+
+type Coord = (Int,Int)
+
+findAsteroidsInLine :: [String] -> Int -> [Coord]
+findAsteroidsInLine grid j = 
+    map (\ix -> (ix,j) ) $ findIndices (=='#') (grid !! j)
+
+allAsteroids :: [String] -> [Coord]    
+allAsteroids grid = 
+    concatMap (findAsteroidsInLine grid) [0 .. length grid -1]
+    
+(>-<) :: Coord -> Coord -> Coord
+(>-<) (x1,y1) (x2,y2) =(x2-x1,y2-y1)
+
+-- Vetor form equation of a line
+lineOfSight :: Coord -> Coord -> [Coord]
+lineOfSight p1 p2 =
+    let
+        (dx,dy) = p1 >-< p2
+        mdc = gcd dx dy
+    in
+        if mdc == 1 then 
+            []
+        else 
+            [((fst p1) + (div dx mdc) * n, (snd p1) + (div dy mdc) * n) | n <- [1 .. (mdc-1)]] 
+
+isVisible :: [String] -> Coord -> Coord -> Bool
+isVisible grid p1 p2 = not $ any (=='#') inLine
+                          
+    where 
+        inLine = map (\(x,y) -> grid !! y !! x) $ lineOfSight p1 p2 
+    
+countAst :: [String] -> Coord -> Int
+countAst grid z = length . filter (== True) . map (isVisible grid z) . filter (/=z) . allAsteroids $ grid
+
+maxCountAll :: [String] -> Int
+maxCountAll grid = maximum $ map (countAst grid) $ allAsteroids grid
+
+main :: IO ()
+main = do 
+    file <- readFile "d10"
+    print $ maxCountAll $ words file
---
+++ Day9.hs
@@ -0,0 +1,79 @@
+module Day9 where
+
+import Text.Regex
+import qualified Data.Text as T
+import Data.List
+
+import Control.Lens 
+
+leadZero opc = getDigits opc ++ [0,0,0,0]
+
+type State = [Integer]
+
+infixr 8 >***<  
+(>***<) :: (a -> b) -> (c -> d) -> (a,c) -> (b,d)
+(>***<) f g = \(a,b) -> (f a, g b)
+
+getDigits :: Integer -> [Integer]
+getDigits n = 
+    unfoldr (\x -> if x == 0 then 
+            Nothing 
+        else 
+            Just . (mod x >***< div x) $ (10,10)) n
+
+getOp :: Integer -> (Integer -> Integer -> Integer)
+getOp 1 = (+) 
+getOp 2 = (*) 
+getOp x = error $ show x
+
+data PgmMode = Normal | Feedback
+
+paramMode :: Integer -> Integer -> Integer -> Integer -> [Integer] -> Integer
+paramMode rb opcode digit inp state 
+    | digit == 4 && res == 0 = inp
+    | res == 2 = state !! fromIntegral (rb + inp)
+    | res == 1 = inp 
+    | res == 0 = state !! fromIntegral inp
+    where 
+        res = leadZero opcode !! (fromIntegral digit)
+        
+runPgm :: Integer -> PgmMode -> Integer -> [Integer] -> Int -> State -> IO (Integer,Int,State,Integer)  
+runPgm rb pm o r ptr state = 
+    let 
+        opcode = state !! ptr
+        inp1 = state   !! (ptr+1)   
+        inp2 = state   !! (ptr+2)
+        out =  state   !! (ptr+3)
+        c = paramMode rb opcode 2 inp1 state 
+        b = paramMode rb opcode 3 inp2 state
+        a = paramMode rb opcode 4 out state
+    in
+        case (mod opcode 100) of
+            3 -> runPgm rb pm o (tail r) (ptr+2) (state & (element $ (fromIntegral c)) .~ (head r))
+            4 -> case pm of  
+                    Normal -> print c >> 
+                              runPgm rb pm c r (ptr+2) state
+                    Feedback -> return (c,ptr+2,state,opcode)
+            5 -> if c /= 0 then runPgm rb pm o r (fromIntegral b) state 
+                           else runPgm rb pm o r (ptr+3) state 
+            6 -> if c == 0 then runPgm rb pm o r (fromIntegral b) state 
+                           else runPgm rb pm o r (ptr+3) state
+            7 -> if c <  b then 
+                    runPgm rb pm o r (ptr+4) (state & (element $ fromIntegral a) .~ 1) 
+                else 
+                    runPgm rb pm o r (ptr+4) (state & (element $ fromIntegral a) .~ 0)
+            8 -> if c == b then 
+                    runPgm rb pm o r (ptr+4) (state & (element $ fromIntegral a) .~ 1) 
+                else 
+                    runPgm rb pm o r (ptr+4) (state & (element $ fromIntegral a) .~ 0)
+            9 -> runPgm (rb + c) pm o r (ptr+2) state
+            99 -> return (o,ptr,take 1000 state,opcode)
+            de -> runPgm rb pm o r (ptr+4) (state & (element $ fromIntegral a) .~ (getOp de c b))
+            
+main :: IO ()
+main = do 
+    file <- readFile "d9"
+    nums <- return . read $ "[" ++ file ++ "]" :: IO [Integer]
+    exec <- runPgm 0 Normal 0 [1] 0 (nums ++ repeat 0)
+    print $ exec
+    
